21668723c964d3972b14774500c5a6332cf86415
Abdou Ly <abdouly@users.noreply.github.com>
2016-11-03 22:27:50 +0100

    commit de la class test

---
+++ src/main/java/dcll/ably/MySimpleStack/SStackTest.java
@@ -0,0 +1,41 @@
+package dcll.ably.MySimpleStack;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ASUS on 03/11/2016.
+ */
+public class SStackTest {
+    SStack stack ;
+    @org.junit.Before
+    public void setUp() throws Exception {
+       stack = new SStack();
+        System.out.println("creation de la pile");
+    }
+
+    @org.junit.Test
+    public void isEmpty() throws Exception {
+        Assert.
+    }
+
+    @org.junit.Test
+    public void getSize() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void push() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void peek() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void pop() throws Exception {
+
+    }
+
+}
